8e5fe9d27688066e4bd36d9736303235830d5aba
MaxMEllon <maxmellon1994@gmail.com>
2017-10-04 14:39:12 +0900

    First commit

---
+++ Gemfile
@@ -0,0 +1,43 @@
+source 'https://rubygems.org'
+
+git_source(:github) do |repo_name|
+  repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
+  "https://github.com/#{repo_name}.git"
+end
+
+gem 'active_model_serializers'
+gem 'coffee-rails', '~> 4.2'
+gem 'devise'
+gem 'devise-i18n'
+gem 'dotenv-rails', require: 'dotenv/rails-now'
+gem 'font-awesome-rails'
+gem 'jbuilder', '~> 2.5'
+gem 'materialize-sass'
+gem 'mysql2', '>= 0.3.18', '< 0.5'
+gem 'puma', '~> 3.7'
+gem 'rack-dev-mark'
+gem 'rails', '~> 5.1.4'
+gem 'sass-rails', '~> 5.0'
+gem 'slim-rails'
+gem 'turbolinks', '~> 5'
+gem 'uglifier', '>= 1.3.0'
+gem 'webpacker'
+
+group :development, :test do
+  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'pry-byebug'
+  gem 'pry-doc'
+  gem 'pry-rails'
+  gem 'rubocop'
+end
+
+group :development do
+  gem 'listen', '>= 3.0.5', '< 3.2'
+  gem 'rspec-rails', '~> 3.6'
+  gem 'spring'
+  gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'web-console', '>= 3.3.0'
+end
+
+# Windows does not include zoneinfo files, so bundle the tzinfo-data gem
+gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ config/application.rb
@@ -0,0 +1,43 @@
+require_relative 'boot'
+
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module OodexNext
+  class Application < Rails::Application
+    config.load_defaults 5.1
+
+    config.generators.system_tests = nil
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.active_record.time_zone_aware_types = %i[datetime time]
+    config.assets.paths << config.root.join('node_modules')
+
+    config.sass.preferred_syntax = :sass
+    config.sass.line_comments = false
+    config.sass.cache = false
+
+    config.rack_dev_mark.enable = !Rails.env.production?
+
+    # Don't generate helper and assets
+    config.generators do |g|
+      g.helper false
+      g.assets false
+
+    config.generators.system_tests = nil
+  end
+end
