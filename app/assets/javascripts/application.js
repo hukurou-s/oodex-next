9cd1b37108557c9a299c8d57668e73795ac305bf
MaxMEllon <maxmellon1994@gmail.com>
2017-10-25 10:43:59 +0900

    Prettier

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,10 +3,10 @@
 //= require nprogress/nprogress
 //= require_tree .
 
-document.addEventListener("turbolinks:click", function() {
-  NProgress.start();
-});
-document.addEventListener("turbolinks:render", function() {
-  NProgress.done();
-  NProgress.remove();
-});
+document.addEventListener('turbolinks:click', function() {
+  NProgress.start()
+})
+document.addEventListener('turbolinks:render', function() {
+  NProgress.done()
+  NProgress.remove()
+})
