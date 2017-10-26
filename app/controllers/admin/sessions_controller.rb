be10d3dab129c04a8c7cae62eb64495ffd7f3f02
MaxMEllon <maxmellon1994@gmail.com>
2017-10-27 00:00:17 +0900

    Fail render session/index when empty session record.

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -6,7 +6,7 @@ class Admin::SessionsController < Admin::ApplicationController
   def index
     sessions = Session.all.order('created_at DESC')
     @q = sessions.ransack(params[:q])
-    @sessions = @q.result.page(params[:page] || 1).per(20)
+    @sessions = @q.result&.page(params[:page] || 1)&.per(20)
   end
 
   def new
@@ -29,7 +29,7 @@ class Admin::SessionsController < Admin::ApplicationController
 
   def show
     @q = @session.missions.ransack(params[:q])
-    @missions = @q.result.page(params[:page] || 1).per(20)
+    @missions = @q.result&.page(params[:page] || 1)&.per(20)
   end
 
   def update
--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -6,6 +6,6 @@ class TopController < ApplicationController
   def index
     sessions = Session.all.order('created_at DESC')
     @q = sessions.ransack(params[:q])
-    @sessions = @q.result.page(params[:page] || 1).per(20)
+    @sessions = @q.result&.page(params[:page] || 1)&.per(20)
   end
 end
