c020d2d4be3035d221de159917b8ebd028ea37f1
MaxMEllon <maxmellon1994@gmail.com>
2017-10-26 18:30:56 +0900

    いいかんじ

--- app/controllers/admin/missions_controller.rb
+++ app/controllers/admin/missions_controller.rb
@@ -1,11 +1,19 @@
 class Admin::MissionsController < Admin::ApplicationController
-  before_action :set_session, only: :new
+  before_action :set_session, only: %i[new]
+  before_action :set_mission, only: %i[edit]
 
   def new
-    @mission = @session.mission.new
+    @mission = @session.missions.new
   end
 
+  def edit; end
+
+
   private
+
+  def set_mission
+    @mission = Mission.find(params[:id])
+  end
 
   def set_session
     @session = Session.find(params[:session_id])
