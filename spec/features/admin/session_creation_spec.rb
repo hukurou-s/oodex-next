57c863ec9c82fbff2af14f60021ca5901f7ea222
MaxMEllon <maxmellon1994@gmail.com>
2017-10-27 05:12:58 +0900

    Add editing session spec.

--- spec/features/admin/session_creation_spec.rb
+++ spec/features/admin/session_creation_spec.rb
@@ -2,21 +2,21 @@
 
 require 'rails_helper'
 
-describe 'user sign in', feature: true do
-  before do
-    login_as create(:user, :admin)
-    visit new_admin_session_path
-    find('.input.name').set('hogehoge')
-    find('.textarea.detail').set("pogepoge\nfoobar")
-    find('.datetime.start_at').set('2017-01-01T00:00+09:00')
-    find('.datetime.end_at').set('2017-12-31T23:59+09:00')
-    find('nav.navbar').click
-  end
+describe 'create session', feature: true do
+  context 'succeeding' do
+    before do
+      login_as create(:user, :admin)
+      visit new_admin_session_path
+      find('.input.name').set('hogehoge')
+      find('.textarea.detail').set("pogepoge\nfoobar")
+      find('.datetime.start_at').set('2017-01-01T00:00+09:00')
+      find('.datetime.end_at').set('2017-12-31T23:59+09:00')
+      find('nav.navbar').click
+    end
 
-  let(:submit) { find('.button.is-link').click }
+    let(:submit) { find('.button.is-link').click }
 
-  context 'succsessful' do
-    it 'expected root path' do
+    it 'expected current path is root path' do
       submit
       expect(page.current_path).to eq(admin_sessions_path)
       session = Session.last
@@ -24,6 +24,20 @@ describe 'user sign in', feature: true do
       expect(session.detail).to eq("pogepoge\r\nfoobar")
       expect(session.start_at).to eq(Time.parse('2017-01-01T00:00+09:00'))
       expect(session.end_at).to eq(Time.parse('2017-12-31T23:59+09:00'))
+    end
+  end
+
+  context 'faulting' do
+    before do
+      login_as create(:user, :admin)
+      visit new_admin_session_path
+    end
+
+    let(:submit) { find('.button.is-link').click }
+
+    it 'expected to fail create session record' do
+      submit
+      expect(page).to have_content('入力してください')
     end
   end
 end
--- spec/features/admin/session_editing_spec.rb
+++ spec/features/admin/session_editing_spec.rb
@@ -2,26 +2,41 @@
 
 require 'rails_helper'
 
-describe 'user sign in', feature: true do
+describe 'edit session', feature: true do
   let(:session) { create :session }
 
-  before do
-    login_as create(:user, :admin)
-    visit edit_admin_session_path(session.id)
-    find('.input.name').set('updated')
-    find('.textarea.detail').set('updated')
-    find('nav.navbar').click
-  end
-
-  let(:submit) { find('.button.is-link').click }
-
   context 'succsessful' do
-    it 'expected root path' do
+    before do
+      login_as create(:user, :admin)
+      visit edit_admin_session_path(session.id)
+      find('.input.name').set('updated')
+      find('.textarea.detail').set('updated')
+      find('nav.navbar').click
+    end
+
+    let(:submit) { find('.button.is-link').click }
+
+    it 'expected current path is root path' do
       submit
       expect(page.current_path).to eq(admin_sessions_path)
       visit admin_session_path(session.id)
       expect(page).to have_content('updated')
       expect(page).to have_content('updated')
+    end
+  end
+
+  context 'faulting' do
+    before do
+      login_as create(:user, :admin)
+      visit edit_admin_session_path(session.id)
+      find('.input.name').set('')
+    end
+
+    let(:submit) { find('.button.is-link').click }
+
+    it 'expected to fail update session record' do
+      submit
+      expect(page).to have_content('入力してください')
     end
   end
 end
