cbfaf60711f27adc41d546a5fc62daef9630518d
MaxMEllon <maxmellon1994@gmail.com>
2018-01-11 18:28:41 +0900

    Modify webpack config

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,22 @@
 const { environment } = require('@rails/webpacker')
+const DashboardPlugin = require('webpack-dashboard/plugin');
+const webpack = require('webpack')
 
-module.exports = environment
+const config = Object.assign({}, environment.toWebpackConfig(), {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?(\.erb)?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: { cacheDirectory: 'tmp/cache/webpacker/babel-loader' }
+      }
+    ]
+  },
+  devtool: process.env.RAILS_ENV === 'development' ? 'cheap-module-eval-source-map' : 'eval',
+})
+
+config.plugins.push(new webpack.NamedModulesPlugin())
+config.plugins.push(new DashboardPlugin())
+
+module.exports = config
