77d1891d97cbeacc81d34d9af297b08fca63a79c
hukurou-s <s18g455@stu.kagawa-u.ac.jp>
2019-08-09 13:54:22 +0900

    fixed position of function declaration

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,9 +5,9 @@ class Test < ApplicationRecord
   belongs_to :pierced_location
   has_many :problem_tests
   has_many :problems, through: :problem_tests
-end
 
-def report_errors
-  logger.info test_errors: errors.full_messages
-  "fail to create test #{errors.full_messages.join(' ')}"
+  def report_errors
+    logger.info test_errors: errors.full_messages
+    "fail to create test #{errors.full_messages.join(' ')}"
+  end
 end
