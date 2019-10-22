aca68d2424548839dbb929da459c92945a8aead2
hukurou-s <s18g455@stu.kagawa-u.ac.jp>
2019-10-22 14:42:32 +0900

    add association

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,5 @@
 class Question < ApplicationRecord
+  belongs_to :problem
+  has_many :question_tests
+  has_many :tests, through: :question_tests
 end
--- app/models/question_test.rb
+++ app/models/question_test.rb
@@ -1,2 +1,5 @@
 class QuestionTest < ApplicationRecord
+  belongs_to :question
+  belongs_to :test
+  enum pierced_level: %i[L0 L1 L2]
 end
