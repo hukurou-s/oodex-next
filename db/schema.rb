2f1b9c876dee9d2239d9da2a246497610e58f401
hukurou-s <s18g455@stu.kagawa-u.ac.jp>
2019-10-22 14:33:18 +0900

    create question model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190612110056) do
+ActiveRecord::Schema.define(version: 20191022051848) do
 
   create_table "missions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "session_id", null: false
@@ -56,6 +56,14 @@ ActiveRecord::Schema.define(version: 20190612110056) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["session_id"], name: "index_projects_on_session_id"
+  end
+
+  create_table "questions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "problem_id", null: false
+    t.string "name", null: false
+    t.text "detail", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "sessions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
---
+++ spec/factories/questions.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :question do
+    problem_id 1
+    name "MyString"
+    detail "MyText"
+  end
+end
