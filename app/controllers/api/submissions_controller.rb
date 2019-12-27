bcc8ba1366cde5c39b25d7c375af4a72207e5ab7
hukurou-s <s18g455@stu.kagawa-u.ac.jp>
2019-12-27 19:48:35 +0900

    implements feature to regist submitted answer

--- app/controllers/api/submissions_controller.rb
+++ app/controllers/api/submissions_controller.rb
@@ -4,33 +4,36 @@ class Api::SubmissionsController < ApplicationController
   before_action :set_question, only: %i[question]
 
   def question
-    logger.debug params[:file_name]
-    logger.debug params[:code]
-
     @submit = Submit.new(submit_params)
-    logger.debug @submit.inspect
-    @s_b = @submit.build_submit_question({
-                                         submit_id: @submit.id,
-                                         question_id: @question.id
-                                       })
-    logger.debug @s_b.inspect
+
+    @s_q = @submit.build_submit_question(
+      submit_id: @submit.id,
+      question_id: @question.id
+    )
+
     pierced_locations = PiercedLocation.where(mission_id: @question.problem.mission)
+    @code = []
     Hash[params[:code].permit!].sort.to_h.each do |key, value|
       pierced_location = pierced_locations.find { |pl| pl.location_id == key.to_i }
-      code = @submit.submit_codes.build({
-                                  submit_id: @submit,
-                                  file_name: params[:file_name],
-                                  code: value,
-                                  pierced_location_id: pierced_location.id
-                                })
-      logger.debug code.inspect
+      @code << @submit.submit_codes.build(
+        submit_id: @submit.id,
+        file_name: params[:file_name],
+        code: value,
+        pierced_location_id: pierced_location.id
+      )
     end
+
+    @submit.save
+    logger.debug @submit.inspect
+    logger.debug @s_q.inspect
+    logger.debug @code.inspect
 
     # create testing job
     render json: {}
   end
 
   private
+
   def submit_params
     {
       user_id: current_user.id,
