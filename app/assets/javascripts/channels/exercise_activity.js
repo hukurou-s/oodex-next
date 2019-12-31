a49a53dd53456b22b468bf6402954fd75f8ede47
hukurou-s <s18g455@stu.kagawa-u.ac.jp>
2019-12-31 18:51:30 +0900

    add preloader

---
+++ app/assets/javascripts/channels/exercise_activity.js
@@ -0,0 +1,32 @@
+const testingCallback = data => {
+  switch (data.status) {
+  case 'testing':
+    window.loader.show('解答をテストしています... 🤔')
+    break
+  case 'fail':
+    window.loader.fail('テストを実行できませんでした 😱')
+    break
+  case 'done':
+    window.loader.done('テストが完了しました 😆')
+    setTimeout(() => {
+      window.loader.hide()
+    }, 1000)
+    break
+  }
+}
+
+const exerciseOptions = {
+  channel: 'ExerciseActivityChannel',
+  //room: 'repo'
+}
+
+window.exerciseActivity = () => {
+  window.App = window.App || {}
+  window.App.cable = ActionCable.createConsumer()
+
+  window.App.repoStream = window.App.cable.subscriptions.create(exerciseOptions, {
+    received(data) {
+      testingCallback(data)
+    }
+  })
+}
